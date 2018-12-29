9b7885c4518d23d470c197095a6604915003b740
Nosheen Makrani <nosheenmakrani@gmail.com>
2018-12-29 16:23:57 +0530

    brand

--- admin/brand_enquiry.php
+++ admin/brand_enquiry.php
@@ -97,7 +97,7 @@ $message = '';
 		<div class="box box-primary">
 
 			<div class="box-header with-border">
-			  <h3 class="box-title">Add Investor Data</h3>
+			  <h3 class="box-title">Add Brand Enquiry</h3>
 				<div class="box-tools pull-right">
 				<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
 				  <i class="fa fa-minus"></i></button>
