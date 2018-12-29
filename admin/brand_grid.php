564d6ed5bc2d4751bbed2a931f09fd6e1ca35154
Dimpaljain <dimpaljain1699@gmail.com>
2018-12-29 16:15:35 +0530

    ho gya

--- admin/brand_grid.php
+++ admin/brand_grid.php
@@ -96,7 +96,7 @@ rel="stylesheet" />
 					<select name="brand_id" class="form-control select2" style="width: 100%;" required>
 					<option selected="selected">Select Brand</option>
 					<?php
-						$query=mysqli_query($db,"select * from brands where status = 'Active'");
+						$query=mysqli_query($db,"select * from brands where status = 'Active' and is_approve='Approved'");
 						while($row=mysqli_fetch_array($query)){
 					?>
 						<option value="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></option>
